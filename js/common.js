7031e77f6a3ac3e6bebe32feac536530e3b871d8
古山 裕一 <y-furuyama@tn-japan.co.jp>
2025-10-14 13:57:44 +0900

    js追加

---
+++ js/common.js
@@ -0,0 +1,9 @@
+function over(target) {
+    // 引数で受け取った要素の背景色をpinkに設定
+    target.style.backgroundColor = "pink";
+}
+
+function leave(target) {
+        // 引数で受け取った要素の背景色を元に戻す（透明にする）
+    target.style.backgroundColor = "transparent";
+}
